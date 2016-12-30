8f410a1ac0d27794f78fea92338f942e642c17c1
Bright <9034785@qq.com>
2016-12-30 22:09:02 +0800

    更新成多线程的方式，瞬间完成trace

--- fast.py
+++ fast.py
@@ -1,23 +1,36 @@
+#!/usr/bin/env python3
+
 import subprocess
 import re
 
 ip = input("Please enter the IP address to trace: ")
 max_ttl = input("Please enter the max hops to trace: ")
-ttl = 1
 
-while ttl <= int(max_ttl):
-    raw_output = subprocess.Popen(['ping', '-c', '1', '-m', str(ttl), '-i', '0.1', '-t', '1', ip], stdout=subprocess.PIPE)
-    output = raw_output.communicate()[0].decode()
-    if 'Time to live exceeded' in output:
-        output_list = output.split('\n')
-        hop = re.search(r'(\d+\.\d+\.\d+\.\d+)', output_list[1]).group(1)
-        print(hop)
-        ttl += 1
-    elif '1 packets received' in output:
-        print(ip)
-        break
-    else:
-        print('*')
-        ttl += 1
-print('End of Trace')
+
+def fast_trace(ip, max_ttl):
+    raw_list = []
+    trace_list = []
+    for i in range(1, int(max_ttl)+1):
+        raw_list.append(subprocess.Popen(['ping', '-c', '1', '-m', str(i), '-i', '0.1', '-t', '1', ip], stdout=subprocess.PIPE))
+    for i in raw_list:
+        output = i.communicate()[0].decode()
+        if 'Time to live exceeded' in output:
+            output_list = output.split('\n')
+            hop = re.search(r'(\d+\.\d+\.\d+\.\d+)', output_list[1]).group(1)
+            trace_list.append(hop)
+        elif '1 packets received' in output:
+            trace_list.append(ip)
+            break
+        else:
+            trace_list.append('*')
+    return trace_list
+
+
+def main():
+    for i in fast_trace(ip, max_ttl):
+        print(i)
+    print('End of Trace')
+
+if __name__ == '__main__':
+    main()
 
